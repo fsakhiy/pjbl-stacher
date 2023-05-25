9e03c2a2d07156e96dfdc52a5bfe36d2993ba52f
fairuzs <fairuz@teknologicakrainternasional.com>
2023-05-25 08:32:30 +0700

    removed unused stuff

--- app/api/staff/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import prisma from "@/app/components/Prisma";
-
-export async function GET(request: Request) {
-  const res = await prisma.staff.findMany()
-  
-  return NextResponse.json(res)
-}
-
