c6615968c93cd078b5a9b95212d4cb3f3bc1cd4f
fairuzs <fairuz@teknologicakrainternasional.com>
2023-05-30 09:28:28 +0700

    added (beta) signup feature

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,10 +1,36 @@
-import Link from "next/link";
+import bcrypt from "bcrypt"
+import { prisma } from "../../components/Prisma"
 
-export default function Signup() {
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Signup() {
+
+  async function createUser(formData: FormData) {
+    "use server"
+
+    if(formData.get("password")!.toString() != formData.get("repeatpassword")!.toString()) {
+      redirect('/auth/signup')
+    } 
+
+    else {
+      let hashedPassword = await bcrypt.hash(formData.get("password")!.toString(), 12)
+
+      await prisma.user.create({data: {
+        name: formData.get("name")!.toString(),
+        email: formData.get("email")!.toString(),
+        password: hashedPassword
+      }})
+
+      
+      redirect("/")
+    }
+  }
+  
   return (
     <div className="w-screen bg-neutral-100">
       <div className="max-w-lg h-screen mx-auto p-10 items-center justify-center flex">
-        <form action="/api/auth/login" method="POST" className="flex flex-col gap-16 w-full">
+        <form action={createUser} className="flex flex-col gap-16 w-full">
           <div className="font-bold text-center text-4xl">
             Signup
           </div>
@@ -12,7 +38,7 @@ export default function Signup() {
             <input type="text" name="name" id="name" placeholder="name" className="p-3 rounded-md border"/>
             <input type="email" name="email" id="email" placeholder="email" className="p-3 rounded-md border"/>
             <input type="password" name="password" id="password" placeholder="password" className="p-3 rounded-md border"/>
-            <input type="password" name="repeat-password" id="repeat-password" placeholder="confirm password" className="p-3 rounded-lg border"/>
+            <input type="password" name="repeatpassword" id="repeatpassword" placeholder="confirm password" className="p-3 rounded-lg border"/>
             {/* <p className="text-end text-blue-800 underline hover:text-blue-400 font-semibold"><Link href='#'>forgot password?</Link></p> */}
           </div>
           <div className="flex flex-col gap-5">
