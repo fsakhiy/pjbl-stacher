dc1b6ac1ab873bbe3754a6d3a587091174ea77b2
fairuzs <fairuz@teknologicakrainternasional.com>
2023-05-30 15:41:20 +0700

    automatic redirect to dashboard (temporary)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
-
+import {redirect} from "next/navigation"
 export default function Home() {
-  return (
-    <div className="bg-neutral-100 h-screen mx-auto flex flex-col justify-center items-center">
-      <p className="font-bold text-3xl text-center">Welcome to stacher</p>
-      <p className="font-semibold text-lg text-center">please <Link className="underline font-bold text-blue-700 hover:text-blue-500" href='/auth/login'>login</Link> or <Link className="underline font-bold text-blue-700 hover:text-blue-500" href='/auth/signup'>signup</Link> to access dashboard</p>
-    </div>  
-  )
+
+  redirect('/dashboard')
+  
+  // return (
+  //   <div className="bg-neutral-100 h-screen mx-auto flex flex-col justify-center items-center">
+  //     <p className="font-bold text-3xl text-center">Welcome to stacher</p>
+  //     <p className="font-semibold text-lg text-center">please <Link className="underline font-bold text-blue-700 hover:text-blue-500" href='/auth/login'>login</Link> or <Link className="underline font-bold text-blue-700 hover:text-blue-500" href='/auth/signup'>signup</Link> to access dashboard</p>
+  //   </div>  
+  // )
 }
